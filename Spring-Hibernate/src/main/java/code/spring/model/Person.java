97da41dca4c5c1bc8d064da0cd047cd4b4a9c5ec
Vineeth <vineeth.madhu@gmail.com>
2021-02-12 01:01:22 -0500

    Changed generations type from Auto to Identity.

--- Spring-Hibernate/src/main/java/code/spring/model/Person.java
+++ Spring-Hibernate/src/main/java/code/spring/model/Person.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 public class Person {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String name;
   private String country;
